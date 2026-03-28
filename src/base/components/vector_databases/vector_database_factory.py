51790976b258960cb187fb80206448acf416c9cd
Jia Ying <jiaying.kam.2024@computing.smu.edu.sg>
2026-03-28 14:51:17 +0800

    lazy import

--- src/base/components/vector_databases/vector_database_factory.py
+++ src/base/components/vector_databases/vector_database_factory.py
@@ -3,9 +3,6 @@ from injector import inject
 from src.common.config import Config
 from src.base.components.embeddings.base import BaseEmbedding as EmbeddingsInterface
 from .base import BaseVectorDatabase
-from .variants.chromadb import ChromaVectorDatabase
-from .variants.inmem import InMemoryVectorDatabase
-from .variants.supabase import SupabaseVectorDatabase
 
 @inject
 def create_vector_database(config: Config, embeddings: EmbeddingsInterface) -> BaseVectorDatabase:
@@ -14,10 +11,13 @@ def create_vector_database(config: Config, embeddings: EmbeddingsInterface) -> B
     """
     vector_database_type = config.vector_database_type.upper() if config.vector_database_type else None
     if vector_database_type == "CHROMA":
+        from .variants.chromadb import ChromaVectorDatabase
         return ChromaVectorDatabase(config, embeddings)
     elif vector_database_type == "INMEM":
+        from .variants.inmem import InMemoryVectorDatabase
         return InMemoryVectorDatabase(config, embeddings)
     elif vector_database_type == "SUPABASE":
+        from .variants.supabase import SupabaseVectorDatabase
         return SupabaseVectorDatabase(config, embeddings)
     else:
         raise ValueError(f"Invalid vector database type: {vector_database_type}")
