b30fee0ba3005e438c49366507cb80585b070962
Jia Ying <jiaying.kam.2024@computing.smu.edu.sg>
2026-03-28 15:04:57 +0800

    remove import

--- src/base/components/vector_databases/__init__.py
+++ src/base/components/vector_databases/__init__.py
@@ -1,9 +1,7 @@
 from .base import BaseVectorDatabase
 from .vector_database_factory import create_vector_database
-from .variants.chromadb import ChromaVectorDatabase
 
 __all__ = [
     "BaseVectorDatabase",
     "create_vector_database",
-    "ChromaVectorDatabase",
 ]
